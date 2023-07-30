cfff589eaa800e11fc5cef23d36d8a1931e65cb2
ohsangsoo <tkdtn3264@gmail.com>
2023-07-30 21:37:00 +0900

    페이스북 ui

---
+++ lib/presentation/facebook_main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FaceBookMain extends StatelessWidget {
+  const FaceBookMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('facebook'),
+        titleTextStyle: const TextStyle(color: Colors.blue , fontSize: 24,fontWeight: FontWeight.bold),
+        backgroundColor: Colors.white
+        ),
+
+
+
+    ) ;
+  }
+}
