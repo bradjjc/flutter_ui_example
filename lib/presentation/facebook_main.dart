6a523ff03a7840d57accf204335c1b6fe2021eef
ohsangsoo <tkdtn3264@gmail.com>
2023-07-30 21:41:42 +0900

    페이스북 텍스트 왼쪽 정렬

--- lib/presentation/facebook_main.dart
+++ lib/presentation/facebook_main.dart
@@ -9,7 +9,8 @@ class FaceBookMain extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         title: Text('facebook'),
-        titleTextStyle: const TextStyle(color: Colors.blue , fontSize: 24,fontWeight: FontWeight.bold),
+        titleTextStyle: const TextStyle(color: Colors.blue , fontSize: 24,fontWeight: FontWeight.bold,),
+        centerTitle: false,
         backgroundColor: Colors.white
         ),
 
