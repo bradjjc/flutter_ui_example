4c5a15a897b53b18bf617cae15d6f1bc373f3d72
ohsangsoo <tkdtn3264@gmail.com>
2023-07-30 21:41:51 +0900

    페이스북 연

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone/presentation/facebook_main.dart';
 import 'package:flutter_clone/presentation/kakao_main.dart';
 
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const kakaoMain(),
+      home: const FaceBookMain(),
     );
   }
 }
