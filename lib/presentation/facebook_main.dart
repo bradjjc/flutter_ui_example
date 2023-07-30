49528c6fbb3b61bb1815e197a1d4f9d90bfd4eb2
ohsangsoo <tkdtn3264@gmail.com>
2023-07-30 21:59:47 +0900

    페이스북 버튼 추가

--- lib/presentation/facebook_main.dart
+++ lib/presentation/facebook_main.dart
@@ -9,13 +9,20 @@ class FaceBookMain extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         title: Text('facebook'),
-        titleTextStyle: const TextStyle(color: Colors.blue , fontSize: 24,fontWeight: FontWeight.bold,),
-        centerTitle: false,
-        backgroundColor: Colors.white
+        titleTextStyle: const TextStyle(
+          color: Colors.blue,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading:ElevatedButton(onPressed: () {  },
+          child: const Icon(Icons.add),
+
+
+        ),
         ),
 
-
-
-    ) ;
+    );
   }
 }
