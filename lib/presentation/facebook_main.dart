da624983d9322c2474db88a3cff06965ce3095c7
ohsangsoo <tkdtn3264@gmail.com>
2023-08-02 17:46:24 +0900

    좋아요 댓글 공유하기버튼 추가하기

--- lib/presentation/facebook_main.dart
+++ lib/presentation/facebook_main.dart
@@ -48,159 +48,173 @@ class FaceBookMain extends StatelessWidget {
           Ink(
             decoration: const ShapeDecoration(
               shape: CircleBorder(),
-              color: Colors.grey,
+              color: Colors.white38,
             ),
             child: IconButton(
               onPressed: () {},
-              icon:  Icon(MdiIcons.facebookMessenger),
+              icon: Icon(MdiIcons.facebookMessenger),
               color: Colors.black,
             ),
           ),
         ],
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 20),
-                child: Ink(
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.account_circle, size: 50),
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 12, right: 70),
-                child: TextButton(
+      body: ListView(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Ink(
+                child: IconButton(
                   onPressed: () {},
-                  child: const Text('무슨생각을 하고계신가요?                   '),
+                  icon: const Icon(Icons.account_circle, size: 50),
+                  color: Colors.grey,
                 ),
               ),
-              IconButton(
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12, right: 70),
+              child: TextButton(
                 onPressed: () {},
-                icon: const Icon(Icons.camera_alt),
+                child: const Text('무슨생각을 하고계신가요?                   '),
               ),
-            ],
-          ),
-          const SizedBox(
-            child: Divider(
-              height: 20,
-              thickness: 7,
-              color: Colors.grey,
             ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.camera_alt),
+            ),
+          ],
+        ),
+        const SizedBox(
+          child: Divider(
+            height: 20,
+            thickness: 7,
+            color: Colors.grey,
           ),
-          Column(
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8, right: 8, top: 4, bottom: 4),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(100, 200),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
+        ),
+        Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 8, top: 4, bottom: 4),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(100, 200),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {},
-                          child: const Text('스토리만들기')),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8, right: 8, top: 4, bottom: 4),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(100, 200),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('스토리만들기')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 8, top: 4, bottom: 4),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(100, 200),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {},
-                          child: const Text('스토리')),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8, right: 8, top: 4, bottom: 4),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(100, 200),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('스토리')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 8, top: 4, bottom: 4),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(100, 200),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {},
-                          child: const Text('스토리')),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8, right: 8, top: 4, bottom: 4),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(100, 200),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('스토리')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 8, top: 4, bottom: 4),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(100, 200),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {},
-                          child: const Text('스토리')),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8, right: 8, top: 4, bottom: 4),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            minimumSize: const Size(100, 200),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('스토리')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 8, top: 4, bottom: 4),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          minimumSize: const Size(100, 200),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {},
-                          child: const Text('스토리')),
-                    ),
-                  ],
-                ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('스토리')),
+                  ),
+                ],
               ),
-            ],
-          ),
-          const SizedBox(
-            child: Divider(
-              height: 20,
-              thickness: 7,
-              color: Colors.grey,
             ),
+          ],
+        ),
+        const SizedBox(
+          child: Divider(
+            height: 20,
+            thickness: 7,
+            color: Colors.grey,
           ),
-          Column(
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  children: [
-                    Container(
+        ),
+        Column(children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Container(
                   width: 400,
                   height: 400,
                   color: Colors.blue,
                 ),
-                    const SizedBox(
-                      child: Divider(
-                        height: 20,
-                        thickness: 7,
-                        color: Colors.grey,
-                      ),
+                const SizedBox(
+                  child: Divider(
+                    height: 20,
+                    thickness: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.thumb_up_alt),
+                      label: const Text('좋아요'),
                     ),
-                    Container(
-                      width: 400,
-                      height: 400,
-                      color: Colors.blue,
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.messenger),
+                      label: const Text('댓글'),
                     ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      label: const Text('공유하기'),
+                      icon: Icon(MdiIcons.shareOutline),
+
+                    ),
+                  ],
+                ),
                 const SizedBox(
                   child: Divider(
                     height: 20,
@@ -208,15 +222,23 @@ class FaceBookMain extends StatelessWidget {
                     color: Colors.grey,
                   ),
                 ),
-                ],
-
-              ),
-              ),
-            ],
+                Container(
+                  width: 400,
+                  height: 400,
+                  color: Colors.blue,
+                ),
+                const SizedBox(
+                  child: Divider(
+                    height: 20,
+                    thickness: 7,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-
+        ]),
+      ]),
     );
   }
 }
