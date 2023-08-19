c5889f9e7701fb261c0e334f2a4d2dc3a2d327ca
ohsangsoo <tkdtn3264@gmail.com>
2023-08-19 16:46:49 +0900

    당근마켓 매안 앱바 완성

--- lib/presentation/facebook_main.dart
+++ lib/presentation/facebook_main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clone/presentation/video/News.dart';
+import 'package:flutter_clone/presentation/video/news.dart';
 import 'package:flutter_clone/presentation/video/video_news.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
